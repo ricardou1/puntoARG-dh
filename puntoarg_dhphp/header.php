5bc5e9263eb354342355f0fcdb25400d795d9e40
Victoria Lastiri <47834621+vikilastiri@users.noreply.github.com>
2019-04-09 16:10:40 -0300

    Update header.php

--- puntoarg_dhphp/header.php
+++ puntoarg_dhphp/header.php
@@ -1,25 +1,22 @@
 <header class="nav-bar">
-  <div class="logotitulo">
-      <a href="index.php"><h1 class="big-title">puntoARG</h1></a>
-  </div>
+  <a href="index.php"><h1 class="big-title">puntoARG</h1></a>
+
   <div class="menu-big">
     <ul class="menu2">
       <li><a href="#">Promociones</a></li>
       <li><a href="#">Destacados</a></li>
-      <li><a href="#">Experiencias</a></li>
+      <li><a href="#">Paquetes</a></li>
     </ul>
   </div>
+
   <div class="menu-small">
     <ul class="menu1">
       <li><a href="#"><i class="fas fa-search"></i></a></li>
-
-      <li> <a href="login.php">
-        <i id="user"class="fas fa-user"></i></a> </li>
-
+      <li> <a href="login.php"><i id="user"class="fas fa-user"></i></a> </li>
       <li> <a href="#"><i id="ayuda"class="far fa-question-circle"></i></a> </li>
-
       <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+      <li><a href="#"><i class="fas fa-bars"></a></i></li>
     </ul>
   </div>
-
 </header>
+
